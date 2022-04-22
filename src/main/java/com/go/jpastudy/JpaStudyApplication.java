58388578644239610e48695e7e2b3f6535a45d30
sosoKim9966 <sosokim9966@gmail.com>
2022-04-23 00:22:01 +0900

    2022/04/22-mariadb

--- src/main/java/com/go/jpastudy/JpaStudyApplication.java
+++ src/main/java/com/go/jpastudy/JpaStudyApplication.java
@@ -2,11 +2,10 @@ package com.go.jpastudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //@EnableJpaAuditing // JPA Auditing 활성화
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class JpaStudyApplication {
 
     public static void main(String[] args) {
